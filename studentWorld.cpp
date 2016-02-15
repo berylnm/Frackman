80e216191052c0405b23e8cec8d9c748e3cfbfd9
berylnm <wby14218@gmail.com>
2016-02-14 18:55:40 -0800

    Update studentWorld.cpp

--- studentWorld.cpp
+++ studentWorld.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+void askactortodosth(Actor* a);
 GameWorld* createStudentWorld(string assetDir)
 {
 	return new StudentWorld(assetDir);
@@ -17,17 +18,28 @@ int StudentWorld::init()
         {
             for (int j=0; j<4; j++)
             {
-                dirt[i][j]= new Dirt(i,j);
+                m_dirt[i][j]= new Dirt(i,j,this);
             }
         }
         else
         {   for (int j=0; j<60; j++)
             {
-                dirt[i][j] = new Dirt(i,j);
+                m_dirt[i][j] = new Dirt(i,j,this);
             }
         
         }
     }
-    frackman = new FrackMan;
+    m_frackman = new FrackMan(this);
     return 1;
+}
+int StudentWorld::move()
+{
+    askactortodosth(m_frackman);
+    decLives();
+    return GWSTATUS_CONTINUE_GAME ;
+    
+}
+void askactortodosth(Actor* a)
+{
+    a->doSomething();
 }
