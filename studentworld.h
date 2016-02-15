f94696dbfb53f0c4b15efbb624f706b62bd54b36
berylnm <wby14218@gmail.com>
2016-02-14 16:23:46 -0800

    Create studentworld.h
    4:22 2/16 ver could output dirt and frackman correctly

---
+++ studentworld.h
@@ -0,0 +1,43 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include "GameConstants.h"
+#include <string>
+#include "Actor.h"
+#include <list>
+using namespace std;
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
+{
+public:
+	StudentWorld(std::string assetDir)
+	 : GameWorld(assetDir)
+	{
+    }
+
+    virtual int init();
+	/*{
+		return GWSTATUS_CONTINUE_GAME;
+	}*/
+
+    virtual int move()
+	{
+		  // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+		  // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+		decLives();
+        return GWSTATUS_PLAYER_WON;
+    }
+
+	virtual void cleanUp()
+	{
+	}
+
+private:
+    list <Actor*> actor;
+    FrackMan* frackman;
+    Dirt* dirt[64][60];
+};
+
+#endif // STUDENTWORLD_H_
