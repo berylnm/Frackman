cb596063c368d165ff6ac6db064b500b235087fa
berylnm <wby14218@gmail.com>
2016-02-14 18:56:03 -0800

    Update actor.cpp

--- actor.cpp
+++ actor.cpp
@@ -3,18 +3,17 @@
 #include "GameConstants.h"
 
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
-Actor::Actor(int imageID, int startX, int startY, Direction dir , double size, unsigned int depth):GraphObject(imageID,startX,startY,dir,size,depth)
-{
-}
+Actor::Actor(int imageID, int startX, int startY, StudentWorld *s, Direction dir , double size, unsigned int depth):GraphObject(imageID,startX,startY,dir,size,depth),m_s(s){}
 StudentWorld* Actor::getWorld()
 {
     return m_s;
 }
+void Actor::doSomething()
+{return;}
 
 
-
-People::People(int imageID,int startX, int startY, Direction dir,int hit)
-:Actor(imageID,startX,startY,dir,1,0),m_h(hit){
+People::People(int imageID,int startX, int startY, StudentWorld *s, Direction dir,int hit)
+:Actor(imageID,startX,startY,s,dir,1,0),m_h(hit){
     setVisible(true);
 }
 int People::geth() const
@@ -30,8 +29,8 @@ void People::boulderhit()
     m_h -= 100;
 }
 
-FrackMan::FrackMan()
-:People(IID_PLAYER,30,60,right,10),m_squirt(5),m_gold(0),m_sonar(1){}
+FrackMan::FrackMan(StudentWorld* s)
+:People(IID_PLAYER,30,60,s,right,10),m_squirt(5),m_gold(0),m_sonar(1){}
 
 void FrackMan::doSomething()
 {
@@ -58,8 +57,8 @@ void FrackMan::doSomething()
 
 
 
-Dirt::Dirt(int startX, int startY)
-:Actor(IID_DIRT,startX,startY,right,0.25,3)
+Dirt::Dirt(int startX, int startY,StudentWorld* s)
+:Actor(IID_DIRT,startX,startY,s,right,0.25,3)
 {
     setVisible(true);
 }
