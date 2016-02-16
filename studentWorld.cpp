a0083ed0ff2a8a2c83306caf2b7f0e764c3a0558
berylnm <wby14218@gmail.com>
2016-02-15 23:43:05 -0800

    Update studentWorld.cpp

--- studentWorld.cpp
+++ studentWorld.cpp
@@ -1,45 +1,100 @@
-#ifndef STUDENTWORLD_H_
-#define STUDENTWORLD_H_
-
-#include "GameWorld.h"
-#include "GameConstants.h"
-#include <string>
+#include "StudentWorld.h"
 #include "Actor.h"
-#include <list>
+#include <string>
 using namespace std;
-// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
-class StudentWorld : public GameWorld
+GameWorld* createStudentWorld(string assetDir)
 {
-public:
-    StudentWorld(std::string assetDir)
-    : GameWorld(assetDir)
-    {
-    }
-    
-    virtual int init();
-    /*{
-     return GWSTATUS_CONTINUE_GAME;
-     }*/
-    
-    virtual int move();
-    /*{
-     // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-     // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-     decLives();
-     return GWSTATUS_PLAYER_WON;
-     }*/
-    
-    virtual void cleanUp();
-    bool cleanf(int x,int y);
-    void creatsquirt(int x, int y,Actor::Direction dir);
-    int getActor(int x,int y);
-    
-    
-private:
-    list <Actor*> m_actor;
-    FrackMan* m_frackman;
-    Dirt* m_dirt[64][60];
-};
+    return new StudentWorld(assetDir);
+}
 
-#endif // STUDENTWORLD_H_
+// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
+
+int StudentWorld::init()
+{
+    for (int i=0; i<64; i++)
+    {   if (i>=30 && i<=33)
+    {
+        for (int j=0; j<4; j++)
+        {
+            m_dirt[i][j]= new Dirt(i,j,this);
+        }
+    }
+    else
+    {   for (int j=0; j<60; j++)
+        {
+        m_dirt[i][j] = new Dirt(i,j,this);
+        }
+        
+        }
+    }
+    m_frackman = new FrackMan(this);
+    m_actor.push_back(m_frackman);
+    m_actor.push_back(new Boulder(14,20,this));
+    cleanf(14,20);
+    
+    return 1;
+}
+int StudentWorld::move()
+{
+    
+    for (list<Actor*>:: iterator p= m_actor.begin(); p!=m_actor.end();p++)
+    {   if((*p)->getState() != 0)
+        (*p)->doSomething();
+    }
+   for (list<Actor*>:: iterator p= m_actor.begin(); p!=m_actor.end();p++)
+   {
+       if ((*p)->getState()== 0)
+       {   delete (*p);
+           p = m_actor.erase(p);
+       }
+
+   }
+    decLives();
+    return GWSTATUS_CONTINUE_GAME ;
+    
+}
+
+void StudentWorld::cleanUp()
+{
+    for (int i=0;i<64;i++)
+    {
+        for (int j=0; j<60; j++)
+            if (m_dirt[i][j]!=nullptr)
+                delete m_dirt[i][j];
+    }
+    delete m_frackman;
+}
+int StudentWorld::getActor(int x, int y)
+{
+    if (m_dirt[x][y]!= nullptr)
+        return 1;
+    else
+        return 0;
+}
+
+
+
+bool StudentWorld::cleanf(int x, int y)
+{   int k = 0;
+    for (int i=0;x+i<64&&i<=3;i++)
+    {   for (int j=0; y+j<60&&j<=3; j++)
+    {
+        if (m_dirt[x+i][y+j] != nullptr)
+        {    delete m_dirt[x+i][y+j];
+            m_dirt[x+i][y+j]= nullptr;
+            k=1;
+        }
+    }
+    }
+    return k;
+}
+void StudentWorld::creatsquirt(int x, int y,Actor::Direction dir)
+{   if (dir == Actor::left)    x=x-4;
+    if (dir == Actor::right)   x=x+4;
+    if (dir == Actor::up)      y=y+4;
+    if (dir == Actor::down)    y=y-4;
+    Squirt *s=new Squirt(x,y,dir,this);
+    playSound(SOUND_PLAYER_SQUIRT);
+    m_actor.push_back(s);
+}
