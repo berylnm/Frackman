a83c48898bc43b691db79ee71005d2eacef7d280
berylnm <wby14218@gmail.com>
2016-02-15 23:39:02 -0800

    Update studentWorld.cpp

--- studentWorld.cpp
+++ studentWorld.cpp
@@ -1,33 +1,45 @@
-#include "StudentWorld.h"
-#include "Actor.h"
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include "GameConstants.h"
 #include <string>
+#include "Actor.h"
+#include <list>
 using namespace std;
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
-GameWorld* createStudentWorld(string assetDir)
+class StudentWorld : public GameWorld
 {
-	return new StudentWorld(assetDir);
-}
-
-// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
-
-int StudentWorld::init()
-{
-    for (int i=0; i<64; i++)
-    {   if (i>=30 && i<=33)
-        {
-            for (int j=0; j<4; j++)
-            {
-                dirt[i][j]= new Dirt(i,j);
-            }
-        }
-        else
-        {   for (int j=0; j<60; j++)
-            {
-                dirt[i][j] = new Dirt(i,j);
-            }
-        
-        }
+public:
+    StudentWorld(std::string assetDir)
+    : GameWorld(assetDir)
+    {
     }
-    frackman = new FrackMan;
-    return 1;
-}
+    
+    virtual int init();
+    /*{
+     return GWSTATUS_CONTINUE_GAME;
+     }*/
+    
+    virtual int move();
+    /*{
+     // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+     // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+     decLives();
+     return GWSTATUS_PLAYER_WON;
+     }*/
+    
+    virtual void cleanUp();
+    bool cleanf(int x,int y);
+    void creatsquirt(int x, int y,Actor::Direction dir);
+    int getActor(int x,int y);
+    
+    
+private:
+    list <Actor*> m_actor;
+    FrackMan* m_frackman;
+    Dirt* m_dirt[64][60];
+};
+
+#endif // STUDENTWORLD_H_
