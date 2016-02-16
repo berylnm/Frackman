e6aea9e3dc980c1dae05c2468fca862b7645a648
berylnm <wby14218@gmail.com>
2016-02-15 23:38:11 -0800

    Update actor.cpp

--- actor.cpp
+++ actor.cpp
@@ -1,65 +1,149 @@
-#include "Actor.h"
-#include "StudentWorld.h"
-#include "GameConstants.h"
+#ifndef ACTOR_H_
+#define ACTOR_H_
 
-// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
-Actor::Actor(int imageID, int startX, int startY, Direction dir , double size, unsigned int depth):GraphObject(imageID,startX,startY,dir,size,depth)
+#include "GraphObject.h"
+class StudentWorld;
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+
+class Actor : public GraphObject
 {
-}
-StudentWorld* Actor::getWorld()
+    public:
+        Actor(int imageID, int startX, int startY,StudentWorld*s,int state,Direction dir = right, double size = 1.0, unsigned int depth = 0);
+        ~Actor(){}
+        virtual void doSomething();
+        int getState() const;
+        void setdead();
+        StudentWorld* getWorld();
+    
+    private:
+        StudentWorld* m_s;
+        int m_state;            //0:dead 1:temporary 2:permanent 3:waiting
+    
+};
+
+class People : public Actor
 {
-    return m_s;
-}
-
-
-
-People::People(int imageID,int startX, int startY, Direction dir,int hit)
-:Actor(imageID,startX,startY,dir,1,0),m_h(hit){
-    setVisible(true);
-}
-int People::geth() const
+    public:
+        People(int imageID,int startX, int startY,StudentWorld*s, Direction dir,int hit);
+        ~People(){}
+        bool isalive();
+        void beannoyed(char ch);
+        //virtual void attck() ;
+    
+    private:
+        int m_h;
+};
+class FrackMan : public People
 {
-    return m_h;
-}
-void People::beannoyed()
+    public:
+        FrackMan(StudentWorld*s);
+        ~FrackMan(){}
+        virtual void doSomething();
+        bool movable(int x, int y, int ch);
+    private:
+        int m_squirt;
+        int m_gold;
+        int m_sonar;
+};
+class Protester : public People
 {
-    m_h -= 2;
-}
-void People::boulderhit()
+    public:
+        Protester(StudentWorld *s);
+        ~Protester();
+    
+};
+class Regularp : public Protester
 {
-    m_h -= 100;
-}
-
-FrackMan::FrackMan()
-:People(IID_PLAYER,30,60,right,10),m_squirt(5),m_gold(0),m_sonar(1){}
-
-void FrackMan::doSomething()
+    
+};
+class Hardcorep: public Protester
 {
-    if (geth() == 0)
-        return;
-    int ch;
-    if (getWorld()->getKey(ch) == true)
-    {
-            switch(ch)// user hit a key this tick! switch (ch)
-            {
-            case KEY_PRESS_LEFT:
-                    moveTo(getX()-1,getY());
-                    break;
-            case KEY_PRESS_RIGHT:
-                    moveTo(getX()+1,getY());
-                    break;
-            case KEY_PRESS_UP:
-                    moveTo(getX(),getY()+1);
-            case KEY_PRESS_DOWN:
-                    moveTo(getX(),getY()-1);
-            }
-    }
-}
-
-
-
-Dirt::Dirt(int startX, int startY)
-:Actor(IID_DIRT,startX,startY,right,0.25,3)
+    
+};
+class Goodies : public Actor
 {
-    setVisible(true);
-}
+    public:
+        Goodies(StudentWorld *s);
+        ~Goodies();
+    private:
+    
+
+};
+class Barrel: public Goodies
+{
+    
+};
+class Gold: public Goodies
+{
+    
+};
+
+class Plsn : public Goodies
+{
+    public:
+        Plsn(StudentWorld *s);
+        ~Plsn();
+    private:
+};
+class Sonar: public Plsn
+{
+    Sonar(StudentWorld *s);
+    ~Sonar();
+};
+class Pool: public Plsn
+{
+    
+};
+class Squirt : public Actor
+{
+    public:
+        Squirt(int x, int y, Direction dir,StudentWorld *s);
+        ~Squirt(){}
+        void doSomething();
+    private:
+        int m_trvl;
+    
+};
+class Boulder: public Actor
+{
+    public:
+        Boulder(int x, int y, StudentWorld* s);
+        ~Boulder(){}
+    void doSomething();
+};
+
+
+
+
+class Dirt : public Actor
+{
+    public:
+        Dirt(int startX, int startY,StudentWorld*s);
+        ~Dirt(){}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // ACTOR_H_
