466f9a8290cb8d16c13611b3dbbb05f28972b7b3
berylnm <wby14218@gmail.com>
2016-02-17 19:33:12 -0800

    Update studentworld.h

--- studentworld.h
+++ studentworld.h
@@ -8,36 +8,45 @@
 #include <list>
 using namespace std;
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+int min(int a, int b);
+int max(int a, int b);
 
 class StudentWorld : public GameWorld
 {
 public:
-	StudentWorld(std::string assetDir)
-	 : GameWorld(assetDir)
-	{
+    StudentWorld(std::string assetDir)
+    : GameWorld(assetDir)
+    {
     }
-
+    
     virtual int init();
-	/*{
-		return GWSTATUS_CONTINUE_GAME;
-	}*/
+    /*{
+     return GWSTATUS_CONTINUE_GAME;
+     }*/
+    
+    virtual int move();
+    /*{
+     // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+     // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+     decLives();
+     return GWSTATUS_PLAYER_WON;
+     }*/
+    
+    virtual void cleanUp();
+    bool existdirt(int x, int y);
+    bool cleanf(int x,int y);
+    void creatsquirt(int x, int y,Actor::Direction dir);
+    bool existfrack(int x, int y);
+    void getSonar();
+    
 
-    virtual int move()
-	{
-		  // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-		  // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-		decLives();
-        return GWSTATUS_PLAYER_WON;
-    }
-
-	virtual void cleanUp()
-	{
-	}
-
+      
+    
+    
 private:
-    list <Actor*> actor;
-    FrackMan* frackman;
-    Dirt* dirt[64][60];
+    list <Actor*> m_actor;
+    FrackMan* m_frackman;
+    Dirt* m_dirt[64][60];
 };
 
 #endif // STUDENTWORLD_H_
